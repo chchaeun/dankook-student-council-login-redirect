43082844cf4c4b1771b549b07260f0b5e0375cfa
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2024-05-18 14:40:29 +0900

    style: 공통 스타일 지정

---
+++ src/style/globalStyles.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  background: #ffffff;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+@media (prefers-color-scheme: dark) {
+  html {
+    color-scheme: dark;
+  }
+  body {
+    color: white;
+    background: black;
+  }
+}
+
+input,
+textarea {
+  -webkit-user-select: auto;
+}
+
+input,
+textarea,
+button {
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  border-radius: 0;
+  -webkit-border-radius: 0;
+  -moz-border-radius: 0;
+}
+
+:focus{
+  outline: solid 2px #0f59d9;
+  border-radius: 1%;
+}
+`;
+
+export default GlobalStyles;
---
+++ src/style/styledComponents.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+const Layout = styled.div`
+  height: calc(var(--vh, 1vh) * 100);
+
+  @media ${({ theme }) => theme.device.large} {
+    width: 480px;
+    margin: 0 auto;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  padding-top: 10rem;
+  background: black;
+  color: white;
+`;
+
+const Titles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-inline: 20px;
+  margin-bottom: 50px;
+
+  gap: 10px;
+
+  h1 {
+    margin: 0;
+  }
+
+  h2 {
+    margin: 0;
+    font-size: 20px;
+  }
+`;
+
+const Contents = styled.div`
+  height: 100%;
+  padding-top: 80px;
+  padding-inline: 20px;
+
+  background: white;
+  border-radius: 1.5rem 1.5rem 0 0;
+  color: black;
+
+  hr {
+    background: lightgray;
+    height: 1px;
+    border: 0;
+  }
+`;
+
+const Button = styled.button<{ disabled?: boolean }>`
+  width: 100%;
+  padding: 20px;
+  font-size: 18px;
+  font-weight: 700;
+  background-color: ${(p) => (p.disabled ? "lightgray" : "black")};
+  color: white;
+  border: none;
+  border-radius: 15px;
+  cursor: ${(p) => (p.disabled ? "default" : "pointer")};
+`;
+
+export { Layout, Container, Titles, Contents, Button };
